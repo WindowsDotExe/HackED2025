1f77ea943fabbae8e836f1b0b973b8d432150b47
Joshua Jimmy <joshuajimmy26@gmail.com>
2025-02-15 20:23:04 -0700

    push for matthew

--- frontend/my-react-app/src/Pages/ChatBot.jsx
+++ frontend/my-react-app/src/Pages/ChatBot.jsx
@@ -1,20 +1,41 @@
 import "../Styles/ChatBot.css";
+import React, { useState } from 'react';
 
 export default function ChatBot() {
+    // Define the messages state as an array of objects
+    const [messages, setMessages] = useState([
+        {
+            text: "What are your future goals?",
+            isBot: true
+        },
+        {
+            text: "I want to be a software engineer",
+            isBot: false
+        }
+    ]);
+
     return (
-        <div>
-            <h1>Interview Prep ChatBot</h1>
-            <p className="message bot">
-            Tell me about yourself
-            </p>
-            <p className="message user">
-            I am a software engineer
-            </p>
-            <form className="input form">
-                {/* This will be the form where users can input their questions and then a call is made to api.post("/api/v1/answers") and in the body w eprovide the answer(input from user) */}
-                <input type="text" placeholder="Enter your question" />
-                <button type="submit">Send</button>
-            </form>
+        <div className="container">
+            <div className="chat-box">
+                <div className="col-1">
+                    <div className="bot-message-box">
+                        <div className="bot-message">
+                            <p> What are your future goals</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-2">
+                    <div className="user-message-box">
+                        <div className="user-message">
+                            <p> I want to be a software engineer</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="input-box">
+                <input className="answer" type="text" placeholder="Type your message here" />
+                <button className="submit">Send</button>
+            </div>
         </div>
     )
 }
